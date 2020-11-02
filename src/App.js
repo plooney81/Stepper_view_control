c172ceac390f46157aeb44da64f3df120cee10d3
Pete Looney <petelooney81@gmail.com>
2020-11-02 11:51:26 -0600

    input type text

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import MyStepper from './MyStepper';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
--- src/myStepper.js
+++ src/myStepper.js
@@ -6,7 +6,8 @@ class MyStepper extends Component{
     constructor(){
         super();
         this.state = {
-            number: 0
+            number: 0,
+            previousVal: 0
         };
     };
 
@@ -17,7 +18,17 @@ class MyStepper extends Component{
     }
 
     handleNewInput = (event) => {
+        let currentNumber = this.state.number;
         
+        console.log(typeof parseInt(event.target.value, 10), parseInt(event.target.value, 10))
+       this.setState({number: event.target.name === 'displayNumber' && isNaN(parseInt(event.target.value, 10)) ? currentNumber : parseInt(event.target.value, 10)})  
+    }
+    empty = (event) => {
+        this.setState({number: '', previousVal: this.state.number})
+    }
+
+    blurredOut = (event) => {
+        this.setState({ number: this.state.number === '' ? this.state.previousVal : this.state.number});
     }
 
 
@@ -27,7 +38,7 @@ class MyStepper extends Component{
                 <Button variant="success" size="sm" name='subtract' onClick={this.handleChange}>
                     -
                 </Button>
-                <input type='number' value={this.state.number} onChange={this.handleNewInput}/>
+                <input type='text' name="displayNumber" value={this.state.number}  onChange={this.handleNewInput} onFocus={this.empty} onBlur={this.blurredOut}/>
                 <Button variant="success" size="sm" name='add' onClick={this.handleChange}>
                     +
                 </Button>
